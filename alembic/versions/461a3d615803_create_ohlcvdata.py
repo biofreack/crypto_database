b913db59b7b0a0e9f579fd2ce709a01045febf8a
Dragos Iacomi <biofreack@gmail.com>
2019-01-18 22:30:49 +0200

    create ohlcvdata table

---
+++ alembic/versions/461a3d615803_create_ohlcvdata.py
@@ -0,0 +1,24 @@
+"""create ohlcvdata
+
+Revision ID: 461a3d615803
+Revises: 
+Create Date: 2019-01-18 21:40:10.759141
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '461a3d615803'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
